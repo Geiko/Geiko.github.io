91fd63afa9f756e16a2c7293e04021eb5462accb
Geiko <k.i.geiko@gmail.com>
2016-06-18 00:52:45 +0300

    animation

---
+++ javascripts/magicAnimation.js
@@ -0,0 +1,80 @@
+$('.magic1').hover(
+	function () {
+  		$(this).addClass('magictime vanishIn');
+	},
+	function () {
+  		$(this).removeClass('magictime vanishIn');
+	}
+);
+
+
+//$('.animate1').addClass('animated flip');
+
+$('.animate1').hover(
+	function () {
+  		$(this).addClass('animated rubberBand');
+	},
+	function() { 
+		$(this).removeClass('animated rubberBand');
+	}
+);
+
+
+$('.animate2').hover(
+	function () {
+  		$(this).addClass('animated shake');
+	},
+	function() { 
+		$(this).removeClass('animated shake');
+	}
+);
+
+
+$('.animate3').hover(
+	function () {
+  		$(this).addClass('animated swing');
+	},
+	function() { 
+		$(this).removeClass('animated swing');
+	}
+);
+
+
+$('.animate4').hover(
+	function () {
+  		$(this).addClass('animated wobble');
+	},
+	function() { 
+		$(this).removeClass('animated wobble');
+	}
+);
+
+
+$('.animate5').hover(
+	function () {
+  		$(this).addClass('animated jello');
+	},
+	function() { 
+		$(this).removeClass('animated jello');
+	}
+);
+
+
+$('.animate6').hover(
+	function () {
+  		$(this).addClass('animated flip');
+	},
+	function() { 
+		$(this).removeClass('animated flip');
+	}
+);
+
+
+$('.animate7').hover(
+	function () {
+  		$(this).addClass('animated flash');
+	},
+	function() { 
+		$(this).removeClass('animated flash');
+	}
+);
