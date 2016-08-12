1d0a1076bb3c316a7f4268769e04d4f0b22c1636
Geiko <k.i.geiko@gmail.com>
2016-08-12 20:38:19 +0300

    intern

--- javascripts/scrolling.js
+++ javascripts/scrolling.js
@@ -1,47 +1,47 @@
-		;$(function(){
-			window.onscroll = function(){
+;$(function(){
+	window.onscroll = function(){
+		if (document.body.scrollTop > 70) {
 
- 				if (document.body.scrollTop > 70) {
+			$('#pulse').removeClass('pulseDown');
 
- 					$('#pulse').removeClass('pulseDown');
+			$('.cv').html(
 
-					$('.cv').html(
-							'<h5 id="project_title" class="animate1">Kostiantyn Geiko &nbsp&nbsp&nbsp Junior .NET/JS Developer</h5>'+
+				'<h5 id="project_title" class="animate1">Kostiantyn Geiko &nbsp&nbsp&nbsp Junior .NET/JS Developer</h5>'+
 
-						'<div class="container">'+
-							'<div class="row">'+
-								'<div class="col-lg-1 col-lg-offset-1">'+
-									'<a href="mailto:k.i.geiko@gmail.com">'+
-										'<div id="pulse" class="animate1"></div>'+
-									'</a>'+
-								'</div>'+
-							'</div>'+
-						'</div>'
-					);
-				}
-				else {
+				'<div class="container">'+
+					'<div class="row">'+
+						'<div class="col-lg-1 col-lg-offset-1">'+
+							'<a href="mailto:k.i.geiko@gmail.com">'+
+								'<div id="pulse" class="animate1"></div>'+
+							'</a>'+
+						'</div>'+
+					'</div>'+
+				'</div>'
+			);
+		}
+		else {
 
- 					$('#pulse').addClass('pulseDown');
+				$('#pulse').addClass('pulseDown');
 
-					$('.cv').html(
+			$('.cv').html(
 
 
-						'<header class="inner">	'+
-							'<h4 id="project_tagline">Curriculum Vitae</h4>'+
-							'<h1 id="project_title" class="animate1">Kostiantyn Geiko</h1>'+
-							'<h1 id="project_title" class="animate1">Junior  .NET/JS Developer</h1>'+
-						'</header>'+
+				'<header class="inner">	'+
+					'<h4 id="project_tagline">Curriculum Vitae</h4>'+
+					'<h1 id="project_title" class="animate1">Kostiantyn Geiko</h1>'+
+					'<h1 id="project_title" class="animate1">Junior  .NET/JS Developer</h1>'+
+				'</header>'+
 
-						'<div class="container">'+
-							'<div class="row">'+
-								'<div class="col-lg-1 col-lg-offset-1">'+
-									'<a href="mailto:k.i.geiko@gmail.com">'+
-										'<div id="pulse" class="animate1"></div>'+
-									'</a>'+
-								'</div>'+
-							'</div>'+
-						'</div>	'
-					);
-				}
-			};
-		});
+				'<div class="container">'+
+					'<div class="row">'+
+						'<div class="col-lg-1 col-lg-offset-1">'+
+							'<a href="mailto:k.i.geiko@gmail.com">'+
+								'<div id="pulse" class="animate1"></div>'+
+							'</a>'+
+						'</div>'+
+					'</div>'+
+				'</div>	'
+			);
+		}
+	};
+});
